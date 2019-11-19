8335191b0d92cd98e90de82bc674d061422e74da
antoHero <veeqanto@gmail.com>
2019-11-19 18:57:57 +0100

    Employees can delete thier gif post

--- backend/controllers/Gif.js
+++ backend/controllers/Gif.js
@@ -23,6 +23,22 @@ const Gif = {
             return res.status(400).send(error);
         }
     },
+    async deleteGif(req, res) {
+        const deleteQuery = 'DELETE FROM gif WHERE id=$1 returning *';
+        try {
+            const { rows } = await db.query(deleteQuery, [req.params.id]);
+            if(!rows[0]) {
+                return res.status(404).send({
+                    'message': 'Oops attempt to remove a gif that does not exist'
+                });
+            }
+            return res.status(204).send({
+                'message': 'Gif deleted'
+            });
+        } catch(error) {
+            return res.status(400).send(error);
+        }
+    }, 
 }
 
 export default Gif;
